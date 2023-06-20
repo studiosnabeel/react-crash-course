e8dd4e4947b5d5a21b17d392a990f249f66c643b
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-21 03:28:02 +0500

    Usestate to set the movie state and prop passing

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,25 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import MovieCard from './components/MovieCard';
 
 const API_KEY = 'www.omdbapi.com?apikey=44ac0d39';
 
+const movie1 = {
+  Title: 'Fighting, Flying and Driving: The Stunts of Spiderman 3',
+  Year: '2007',
+  imdbID: 'tt1132238',
+  Type: 'movie',
+  Poster:
+    'https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg',
+};
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`https://${API_KEY}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -28,16 +39,24 @@ const App = () => {
             placeholder="search by name"
             onChange={() => {}}
           />
-          <img className="w-[20px]" src="./search.svg" alt="search-logo" />
+          <img
+            className="w-[20px]"
+            src="./search.svg"
+            alt="search-logo"
+            onClick={() => {}}
+          />
         </div>
-        <div className="">
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-        </div>
+        {movies?.length > 0 ? (
+          <div className="w-full flex flex-wrap justify-center items-center gap-8">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div>
+            <h2>No movies found</h2>
+          </div>
+        )}
       </div>
     </>
   );

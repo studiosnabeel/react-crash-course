e48f60acf28cb112a58ff2c8582d2159b8f02ab8
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-21 03:26:45 +0500

    add jsx for moviecard in component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
-const MovieCard = () => {
-  return <div className="mt-8">MovieCard</div>;
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="">
+      <div className="relative mt-8">
+        <div className="absolute top-0 bg-transparent font-semibold text-gray-700">
+          {movie.Year}
+        </div>
+        <div>
+          <img
+            className="w-[400px] h-[400px]"
+            alt={movie.Title}
+            src={
+              movie.Poster !== 'N/A'
+                ? movie.Poster
+                : 'https://via.placeholder.com/400'
+            }
+          />
+        </div>
+        <div className=" text-white font-semibold text-lg">
+          <span className="absolute bottom-14 bg-transparent text-gray-700">
+            {movie.Type}
+          </span>
+          <h3 className="absolute bottom-0 bg-gray-700 font-bold text-white">
+            {movie.Title}
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;

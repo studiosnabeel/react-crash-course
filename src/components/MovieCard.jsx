77d45bf4c92e60edeb020a0eac71e5fd78f3f0f0
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-21 03:53:34 +0500

    Destructure movie

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie: { Year, Title, Poster, Type, imdbID } }) => {
   return (
-    <div className="">
-      <div className="relative mt-8">
-        <div className="absolute top-0 bg-transparent font-semibold text-gray-700">
-          {movie.Year}
-        </div>
-        <div>
-          <img
-            className="w-[400px] h-[400px]"
-            alt={movie.Title}
-            src={
-              movie.Poster !== 'N/A'
-                ? movie.Poster
-                : 'https://via.placeholder.com/400'
-            }
-          />
-        </div>
-        <div className=" text-white font-semibold text-lg">
-          <span className="absolute bottom-14 bg-transparent text-gray-700">
-            {movie.Type}
-          </span>
-          <h3 className="absolute bottom-0 bg-gray-700 font-bold text-white">
-            {movie.Title}
-          </h3>
-        </div>
+    <div className="relative mt-8" key={imdbID}>
+      <div className="absolute top-0 bg-transparent font-semibold text-gray-700">
+        {Year}
+      </div>
+      <div>
+        <img
+          className="w-[400px] h-[400px]"
+          alt={Title}
+          src={Poster !== 'N/A' ? Poster : 'https://via.placeholder.com/400'}
+        />
+      </div>
+      <div className=" text-white font-semibold text-lg">
+        <span className="absolute bottom-14 bg-transparent text-gray-700">
+          {Type}
+        </span>
+        <h3 className="absolute bottom-0 bg-gray-700 font-bold text-white">
+          {Title}
+        </h3>
       </div>
     </div>
   );
